22a2e676ccfe648c00f20825176aae2dbf0b0003
EndrioAlberton <endrio.alberton@gmail.com>
2025-04-19 11:11:57 -0300

    feat: adicionado interface da página de login

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,10 +1,44 @@
+'use client';
+import { useState } from "react";
+
 export default function Login() {
-    return (
-      <div>
-        <h1 className="text-2xl font-bold">Login</h1>
-        <p>Conteúdo da página do Login</p>
+
+  const [email, setEmail] = useState('');
+  const [password, SetPassword] = useState('');
+
+
+  const logar = () => {
+    alert(`Logado com sucesso! E-mail: ${email}, Senha: ${password}`);
+  };
+
+  return (
+    <div className="max-w-md mx-auto p-6 bg-[#f5f5f5] rounded-lg shadow-md">
+      <h1 className="text-2xl font-bold mb-4 text-center">Login</h1>
+      <div className="space-y-4">
+        <input
+          type="email "
+          placeholder="Digite o e-mail"
+          className="w-full p-2 border border-gray-300 rounded"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Digite a senha"
+          className="w-full p-2 border border-gray-300 rounded"
+          value={password}
+          onChange={(e) => SetPassword(e.target.value)}
+        />
+        <button
+          onClick={logar}
+          className="w-full bg-[#4e342e] text-white py-2 rounded hover:bg-[#5d4037]"
+        >
+          Calcular
+        </button>
       </div>
-    );
-  }
+
+    </div>
+  );
+}
   
   
